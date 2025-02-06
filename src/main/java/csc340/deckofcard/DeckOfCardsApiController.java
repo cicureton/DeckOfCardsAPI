a51a145939a93d36a7601c425c63d93e1e859dd5
cicureton <ccinfinity05@gmail.com>
2025-02-05 21:58:59 -0500

    Update DeckOfCardsApiController.java

--- src/main/java/csc340/deckofcard/DeckOfCardsApiController.java
+++ src/main/java/csc340/deckofcard/DeckOfCardsApiController.java
@@ -12,13 +12,16 @@ public class DeckOfCardsApiController {
     @GetMapping("/draw")
     public DeckResponse drawCards(@RequestParam(defaultValue = "2") int count) {
         RestTemplate restTemplate = new RestTemplate();
+
+        // Create a new deck and get it's the id of a deck
         String newDeckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
         DeckResponse newDeck = restTemplate.getForObject(newDeckUrl, DeckResponse.class);
 
         if (newDeck == null || newDeck.deck_id == null) {
             throw new RuntimeException("Failed to create a new deck.");
         }
-
+        
+        //Use the deck id to draw cards
         String drawUrl = "https://deckofcardsapi.com/api/deck/" + newDeck.deck_id + "/draw/?count=" + count;
         return restTemplate.getForObject(drawUrl, DeckResponse.class);
     }
